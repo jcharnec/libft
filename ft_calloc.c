1719f19433252188420c8bd54a846ad224e8398b
jcharnec <xjpeterx@gmail.com>
2022-09-25 22:34:05 +0200

    add malloc

---
+++ ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 22:29:20 by marvin            #+#    #+#             */
+/*   Updated: 2022/09/25 22:29:20 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void
+	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void*)malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count);
+	return (ptr);
+}
