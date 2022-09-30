e6abae5006339f27e48040146176320fad8b2416
jcharnec <xjpeterx@gmail.com>
2022-09-30 13:20:32 +0200

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -14,13 +14,13 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*ptr;
+	size_t	tot_size;
+	void	*dst;
 
-	ptr = (void *)malloc(count * size);
-	if (!ptr)
-	{
-		return (NULL);
-	}
-	ft_bzero(ptr, count);
-	return (ptr);
+	tot_size = size * count;
+	if (!(dst = malloc(tot_size)))
+		return (0);
+	ft_memset(dst, 0, tot_size);
+	return (dst);
 }
+
