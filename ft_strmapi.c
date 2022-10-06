7b67fda9aad1388ea573280e12482fbe45e92254
jcharnec <xjpeterx@gmail.com>
2022-10-06 10:33:16 +0200

    commit adicional

---
+++ ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 10:22:56 by marvin            #+#    #+#             */
+/*   Updated: 2022/10/06 10:22:56 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	len = ft_strlen(s);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	while (i < len)
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[len] = '\0';
+	return (str);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -10,13 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/* El prototipo de la función substr es: char* substr(const char *source, int m, int n)
-
-Devuelve la subcadena de la cadena de origen que comienza en la posición m y terminando 
-en la posición n-1. */
+/*	DESCRIPCIÓN 
+	Asigna (con malloc(3)) y devuelve una subcadena de la cadena 's'. 
+	La subcadena comienza en el índice 'inicio' y tiene el tamaño máximo 'largo'. 
+	PARÁMETROS * #1. La cadena a partir de la cual crear la subcadena. 
+	#2. El índice de inicio de la subcadena en la cadena 's'. 
+	#3. La longitud máxima de la subcadena. 
+	VALORES DEVUELTOS 
+	La subcadena. NULL si falla la asignación.. */
 
 #include "libft.h"
+#include <stdlib.h>
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*s2;
+	size_t	s_len;
+	size_t	end;
+
+	if (!s)
+		return (0);
+	s_len = ft_strlen(s);
+	end = 0;
+	if (start < s_len)
+		end = s_len - start;
+	if (end > len)
+		end = len;
+	s2 = (char *)malloc(sizeof(char) * (end + 1));
+	if (!s2)
+		return (0);
+	ft_strlcpy(s2, s + start, end + 1);
+	return (s2);
+}
+
+/*
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
@@ -32,3 +59,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str[j] = '\0';
 	return (str);
 }
+*/
